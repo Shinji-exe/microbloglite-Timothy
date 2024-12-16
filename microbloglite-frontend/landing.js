7f1140216fcef90e02a171126bc866fc8f072897
Timothy <timlundy74@gmail.com>
2024-12-15 19:13:13 -0500

    Make user

--- microbloglite-frontend/landing.js
+++ microbloglite-frontend/landing.js
@@ -3,22 +3,82 @@
 "use strict";
 
 const loginForm = document.querySelector("#login");
+const forms = document.getElementById("forms");
+const fullName = document.getElementById("fullName")
+const usernameField = document.getElementById("usernameField")
+const emailField = document.getElementById("emailField")
 
 loginForm.onsubmit = function (event) {
-    // Prevent the form from refreshing the page,
-    // as it will do by default when the Submit event is triggered:
-    event.preventDefault();
+  // Prevent the form from refreshing the page,
+  // as it will do by default when the Submit event is triggered:
+  event.preventDefault();
 
-    // We can use loginForm.username (for example) to access
-    // the input element in the form which has the ID of "username".
-    const loginData = {
-        username: loginForm.username.value,
-        password: loginForm.password.value,
-    }
+  // We can use loginForm.username (for example) to access
+  // the input element in the form which has the ID of "username".
+  const loginData = {
+    username: loginForm.username.value,
+    password: loginForm.password.value,
+  };
 
-    // Disables the button after the form has been submitted already:
-    loginForm.loginButton.disabled = true;
+  // Disables the button after the form has been submitted already:
+  loginForm.loginButton.disabled = true;
 
-    // Time to actually process the login using the function from auth.js!
-    login(loginData);
+  // Time to actually process the login using the function from auth.js!
+  login(loginData);
 };
+
+async function makeProfile(event) {
+  event.preventDefault();
+  let signUpData = {
+    fullName: forms.fullName.value.trim(),
+    username: forms.usernameField.value.trim(),
+    email: forms.emailField.value.trim(),
+    password: forms.passwordField.value.trim(),
+    bio: forms.bio.value.trim(),
+  };
+
+  try {
+    let promise = fetch("http://localhost:5005/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(signUpData),
+    });
+    let response = await promise;
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error code", error.message);
+  }
+}
+
+let darkmode = localStorage.getItem("darkmode");
+let themeChanger = document.getElementById("themeChanger");
+
+function enableDarkmode() {
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+  themeChanger.innerText = "Light";
+}
+
+function disableDarkmode() {
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+  themeChanger.innerText = "Dark";
+}
+
+if (darkmode === "active") enableDarkmode();
+
+themeChanger.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  // darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+  // console.log(themeChanger)
+  if (darkmode !== "active") {
+    enableDarkmode();
+  } else {
+    disableDarkmode();
+  }
+});
+
+console.log(themeChanger);
--- microbloglite-frontend/posts.js
+++ microbloglite-frontend/posts.js
@@ -1,3 +1,120 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+let postCardArea = document.getElementById("postCardArea");
+
+async function getAllPost() {
+  const loginData = getLoginData();
+  try {
+    let promise = fetch("http://localhost:5005/api/posts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${loginData.token}`,
+      },
+    });
+    let response = await promise;
+    let data = await response.json();
+    console.log(data);
+    createPostCards(data);
+  } catch (error) {
+    console.error;
+  }
+}
+
+getAllPost();
+
+async function getAPost(posts) {
+  const loginData = getLoginData();
+  try {
+    let promise = fetch(`http://localhost:5005/api/posts/${posts.id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${loginData.token}`,
+      },
+    });
+    let response = await promise;
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error;
+  }
+}
+
+async function createAPost() {
+  const loginData = getLoginData();
+  let newPost = {
+    text: document.getElementById("tweetInput").value.trim(),
+  };
+  try {
+    let promise = fetch("http://localhost:5005/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${loginData.token}`,
+      },
+      body: JSON.stringify(newPost),
+    });
+    let response = await promise;
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error;
+  }
+}
+
+function createPostCards(posts) {
+  posts.forEach((post) => {
+    let card = document.createElement("div");
+    card.className = "card my-3";
+
+    let cardHeader = document.createElement("div");
+    cardHeader.className = "card-header";
+    card.appendChild(cardHeader);
+
+    let cardHeaderProfileName = document.createElement("a")
+    cardHeaderProfileName.href = `userProfile.html?id=${post._id}`
+    cardHeaderProfileName.innerText =  post.username
+    cardHeader.appendChild(cardHeaderProfileName)
+
+    let imagePost = document.createElement("img");
+    imagePost.src = post.imagePost;
+    card.appendChild(imagePost);
+
+    let cardBody = document.createElement("div");
+    let cardText = document.createElement("p");
+    cardText.innerText = post.text;
+    cardBody.appendChild(cardText);
+    card.appendChild(cardBody);
+
+    let likeButton = document.createElement("button")
+    likeButton.innerText = "Like"
+    likeButton.addEventListener("click", async ()=> {
+        
+    })
+
+    let cardFooter = document.createElement("div");
+    cardFooter.className = "card-footer"
+    let cardFooterText = document.createElement("p");
+    cardFooterText.innerText = new Date(post.createdAt);
+    cardFooter.appendChild(cardFooterText);
+    card.appendChild(cardFooter);
+
+    postCardArea.appendChild(card);
+  });
+}
+
+async function filterPost(params) {
+  let searchValue = placehold.value;
+  try {
+    let promise = fetch("http://localhost:5005/api/posts");
+    let response = await promise;
+    let data = await response.json();
+    console.log(data);
+    let filteringPost = data.filter((post) => post);
+  } catch (error) {
+    console.error;
+  }
+}
