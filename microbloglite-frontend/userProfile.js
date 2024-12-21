0d3fddbc3c73d1f09df0bcf854027f0096560f08
Shinji-exe <67288133+Shinji-exe@users.noreply.github.com>
2024-12-20 19:04:37 -0500

    Add files via upload

--- microbloglite-frontend/userProfile.js
+++ microbloglite-frontend/userProfile.js
@@ -6,6 +6,7 @@ let theUsername = document.getElementById("theUsername");
 let biography = document.getElementById("biography");
 let pfp = document.querySelector("#pfp");
 let proPic = document.getElementById("proPic");
+let theFullName = document.getElementById("theFullName")
 const urlParams = new URLSearchParams(location.search);
 let username = urlParams.get("username");
 // let profileName = localStorage.getItem("username");
@@ -30,7 +31,8 @@ async function getProfile() {
     let response = await promise;
     let data = await response.json();
     console.log(data);
-    theUsername.innerText = loginData.username || "Loading...";
+    theUsername.innerText = data.username || "Loading...";
+    theFullName.innerText = `@${data.fullName}` || "Loading"
     biography.innerText = data.bio;
     proPic.src = data.profilePicture || "images/l60Hf.png";
     proPic.className = "proPic";
@@ -64,185 +66,238 @@ async function getAllUserPosts() {
 getAllUserPosts();
 
 function createPostCards(posts) {
-  posts.forEach((post) => {
-    let feed = document.createElement("div");
-    feed.className = "feed";
+  const postCardArea = document.getElementById("postCardArea");
 
-    let feedHeader = document.createElement("div");
-    feedHeader.className = "feed__header";
+  posts.forEach(() => {
+    let skeletonStructure = document.createElement("div");
+    skeletonStructure.className = "post card my-3 border skeleton";
 
-    let postStructure = document.createElement("div");
-    postStructure.className = "post card my-3 border w-100";
+    let skeletonHeader = document.createElement("div");
+    skeletonHeader.className = "d-flex align-items-center p-3";
+    let skeletonAvatar = document.createElement("div");
+    skeletonAvatar.className = "skeleton skeleton-text";
+    skeletonAvatar.style.width = "50px";
+    skeletonAvatar.style.height = "50px";
+    skeletonAvatar.style.borderRadius = "50%";
 
-    let usernameAndPic = document.createElement("div");
-    usernameAndPic.className = "d-flex align-items-center";
+    let skeletonUsername = document.createElement("div");
+    skeletonUsername.className = "skeleton skeleton-text";
+    skeletonUsername.style.width = "150px";
 
-    let postAvatar = document.createElement("div");
-    postAvatar.className = "post__avatar";
+    let skeletonTime = document.createElement("div");
+    skeletonTime.className = "skeleton skeleton-text";
+    skeletonTime.style.width = "100px";
 
-    let postImageProfile = document.createElement("img");
-    postImageProfile.src = post.imageProfile ? post.imageProfile : "images/l60Hf.png";
-    postAvatar.appendChild(postImageProfile);
+    skeletonHeader.appendChild(skeletonAvatar);
+    skeletonHeader.appendChild(skeletonUsername);
+    skeletonHeader.appendChild(skeletonTime);
 
-    let userNameLink = document.createElement("a");
-    userNameLink.href = `userProfile.html?username=${post.username}`;
-    userNameLink.className = "post__username";
-    userNameLink.innerText = post.username ? post.username : "Unknown User/Variant";
+    let skeletonBody = document.createElement("div");
+    skeletonBody.className = "p-3";
+    let skeletonText = document.createElement("div");
+    skeletonText.className = "skeleton skeleton-text";
+    skeletonText.style.height = "1rem";
 
-    let timeOfCreation = document.createElement("span");
-    timeOfCreation.className = "ps-3";
-    timeOfCreation.innerText = new Date(post.createdAt).toLocaleString();
-
-    profileTitle.href = `userProfile.html?username=${post.username}`;
-
-    usernameAndPic.appendChild(postAvatar);
-    usernameAndPic.appendChild(userNameLink);
-    usernameAndPic.appendChild(timeOfCreation);
-
-    let postBody = document.createElement("div");
-    postBody.className = "post__body";
-    let postHeader = document.createElement("div");
-    postHeader.className = "post__header";
-
-    let postHeaderText = document.createElement("div");
-    postHeaderText.className = "post__headerText";
-
-    let text = document.createElement("a");
-    text.innerText = `${post.text}`;
-    text.style.color = "black";
-    text.style.textDecoration = "none";
-    text.href = `postDetail.html?_id=${post._id}`;
-
-    let headerSpecial = document.createElement("span");
-    headerSpecial.className = "post__headerSpecial";
-
-    let options = document.createElement("div");
-    options.className = "container mt-3 d-flex justify-content-around border-top";
-
-    let likeButton = document.createElement("button");
-    likeButton.innerHTML = '<i class="bi bi-hand-thumbs-up"></i>';
-    likeButton.className = "icons ms-3 btn";
-    likeButton.addEventListener("click", async () => {
-      const loginData = getLoginData();
-      let like = {
-        postId: `${post._id}`,
-      };
-      try {
-        let promise = fetch(`http://localhost:5005/api/likes`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${loginData.token}`,
-          },
-          body: JSON.stringify(like),
-        });
-        let response = await promise;
-        let data = await response.json();
-        console.log(data);
-        location.reload(true);
-      } catch (error) {
-        console.error;
-      }
-      // let promise = fetch("http://localhost:5005/api/likes" + post._id);
-    });
-
-    let likeCounter = document.createElement("span");
-    likeCounter.innerText = post.likes.length;
-    likeCounter.className = "ps-2";
-
-    if (post.likes.length > 0) {
-      likeButton.style.color = "#1a91da";
+    let skeletonActions = document.createElement("div");
+    skeletonActions.className = "d-flex justify-content-around mt-3";
+    for (let i = 0; i < 4; i++) {
+      let skeletonAction = document.createElement("div");
+      skeletonAction.className = "skeleton skeleton-text";
+      skeletonAction.style.width = "30px";
+      skeletonAction.style.height = "30px";
+      skeletonActions.appendChild(skeletonAction);
     }
+    skeletonBody.appendChild(skeletonText);
+    skeletonStructure.appendChild(skeletonHeader);
+    skeletonStructure.appendChild(skeletonBody);
+    skeletonStructure.appendChild(skeletonActions);
+    postCardArea.appendChild(skeletonStructure);
+  });
 
-    likeButton.appendChild(likeCounter);
+  setTimeout(() => {
+    postCardArea.innerHTML = "";
+    posts.forEach((post) => {
+      let feed = document.createElement("div");
+      feed.className = "feed";
 
-    let comment = document.createElement("button");
-    comment.innerHTML = `<i class="bi bi-chat"></i>`;
-    comment.className = "btn";
+      let feedHeader = document.createElement("div");
+      feedHeader.className = "feed__header";
 
-    let commentCounter = document.createElement("span");
-    commentCounter.innerText = "0";
-    commentCounter.className = "ps-2";
+      let postStructure = document.createElement("div");
+      postStructure.className = "post card my-3 border";
 
-    comment.appendChild(commentCounter);
+      let usernameAndPic = document.createElement("div");
+      usernameAndPic.className = "d-flex align-items-center";
 
-    let retweet = document.createElement("button");
-    retweet.innerHTML = `<i class="bi bi-arrow-repeat"></i>`;
-    retweet.className = "btn";
+      let postAvatar = document.createElement("div");
+      postAvatar.className = "post__avatar";
 
-    let retweetCounter = document.createElement("span");
-    retweetCounter.innerText = "0";
-    retweetCounter.className = "ps-2";
+      let postImageProfile = document.createElement("img");
+      postImageProfile.className = "skeleton";
+      postImageProfile.src = post.imageProfile ? post.imageProfile : "images/l60Hf.png";
+      postAvatar.appendChild(postImageProfile);
 
-    retweet.appendChild(retweetCounter);
+      let userNameLink = document.createElement("a");
+      userNameLink.href = `userProfile.html?username=${post.username}`;
+      userNameLink.className = "post__username";
+      userNameLink.innerText = post.username ? post.username : "Unknown User/Variant";
 
-    let upload = document.createElement("button");
-    upload.innerHTML = `<i class="bi bi-upload"></i>`;
-    upload.className = "btn";
+      let timeOfCreation = document.createElement("span");
+      timeOfCreation.className = "ps-3";
+      timeOfCreation.innerText = new Date(post.createdAt).toLocaleString();
 
-    options.appendChild(likeButton);
-    options.appendChild(comment);
-    options.appendChild(retweet);
-    options.appendChild(upload);
+      profileTitle.href = `userProfile.html?username=${post.username}`;
 
-    let headerDescription = document.createElement("div");
-    headerDescription.className = "post__headerDescription";
+      usernameAndPic.appendChild(postAvatar);
+      usernameAndPic.appendChild(userNameLink);
+      usernameAndPic.appendChild(timeOfCreation);
 
-    let editPost = document.createElement("button");
-    editPost.innerText = "Edit";
-    editPost.className = "btn btn-dark me-2 rounded-5";
-    editPost.addEventListener("click", () => {
-      location.href = `editPost.html?_id=${post._id}`;
-    });
+      let postBody = document.createElement("div");
+      postBody.className = "post__body";
+      let postHeader = document.createElement("div");
+      postHeader.className = "post__header";
 
-    // if (post?.username === user?.username) {
-    let deleteButton = document.createElement("button");
-    deleteButton.innerText = `Delete`;
-    deleteButton.className = "btn btn-dark rounded-5";
+      let postHeaderText = document.createElement("div");
+      postHeaderText.className = "post__headerText";
 
-    deleteButton.addEventListener("click", async () => {
-      if (post._id) {
+      let text = document.createElement("a");
+      text.innerText = `${post.text}`;
+      text.style.color = "black";
+      text.style.textDecoration = "none";
+      text.href = `postDetail.html?_id=${post._id}`;
+
+      let headerSpecial = document.createElement("span");
+      headerSpecial.className = "post__headerSpecial";
+
+      let options = document.createElement("div");
+      options.className = "container mt-3 d-flex justify-content-around border-top";
+
+      let likeButton = document.createElement("button");
+      likeButton.innerHTML = '<i class="bi bi-hand-thumbs-up"></i>';
+      likeButton.className = "icons ms-3 btn";
+      likeButton.addEventListener("click", async () => {
         const loginData = getLoginData();
+        let like = {
+          postId: `${post._id}`,
+        };
         try {
-          let promise = fetch(`http://localhost:5005/api/posts/${post._id}`, {
-            method: "DELETE",
+          let promise = fetch(`http://localhost:5005/api/likes`, {
+            method: "POST",
             headers: {
               "Content-Type": "application/json",
               Authorization: `Bearer ${loginData.token}`,
             },
+            body: JSON.stringify(like),
           });
-
-          if (response.ok) {
-            console.log("Success");
-            location.reload();
-          }
-
           let response = await promise;
           let data = await response.json();
           console.log(data);
+          location.reload(true);
         } catch (error) {
           console.error;
         }
-      }
-    });
-    feed.appendChild(feedHeader);
+        // let promise = fetch("http://localhost:5005/api/likes" + post._id);
+      });
 
-    feed.appendChild(postStructure);
-    postHeaderText.appendChild(text);
-    postHeader.appendChild(postHeaderText);
-    postHeader.appendChild(headerSpecial);
-    postBody.appendChild(postHeader);
-    postBody.appendChild(headerDescription);
-    postBody.appendChild(editPost);
-    postBody.appendChild(deleteButton);
-    // postStructure.appendChild(postAvatar);
-    // postStructure.appendChild(userNameLink);
-    postStructure.appendChild(usernameAndPic);
-    postStructure.appendChild(postBody);
-    postStructure.appendChild(options);
-    postCardArea.appendChild(postStructure);
-  });
-  
+      let likeCounter = document.createElement("span");
+      likeCounter.innerText = post.likes.length;
+      likeCounter.className = "ps-2";
+
+      if (post.likes.length > 0) {
+        likeButton.style.color = "#1a91da";
+      }
+
+      likeButton.appendChild(likeCounter);
+
+      let comment = document.createElement("button");
+      comment.innerHTML = `<i class="bi bi-chat"></i>`;
+      comment.className = "btn";
+
+      let commentCounter = document.createElement("span");
+      commentCounter.innerText = "0";
+      commentCounter.className = "ps-2";
+
+      comment.appendChild(commentCounter);
+
+      let retweet = document.createElement("button");
+      retweet.innerHTML = `<i class="bi bi-arrow-repeat"></i>`;
+      retweet.className = "btn";
+
+      let retweetCounter = document.createElement("span");
+      retweetCounter.innerText = "0";
+      retweetCounter.className = "ps-2";
+
+      retweet.appendChild(retweetCounter);
+
+      let upload = document.createElement("button");
+      upload.innerHTML = `<i class="bi bi-upload"></i>`;
+      upload.className = "btn";
+
+      options.appendChild(likeButton);
+      options.appendChild(comment);
+      options.appendChild(retweet);
+      options.appendChild(upload);
+
+      let headerDescription = document.createElement("div");
+      headerDescription.className = "post__headerDescription";
+
+      let editPost = document.createElement("button");
+      editPost.innerText = "Edit";
+      editPost.className = "btn btn-dark me-2 rounded-5";
+      editPost.addEventListener("click", () => {
+        location.href = `editPost.html?_id=${post._id}`;
+      });
+
+      // if (post?.username === user?.username) {
+      let deleteButton = document.createElement("button");
+      deleteButton.innerText = `Delete`;
+      deleteButton.className = "btn btn-dark rounded-5";
+
+      deleteButton.addEventListener("click", async () => {
+        if (post._id) {
+          const loginData = getLoginData();
+          try {
+            let promise = fetch(`http://localhost:5005/api/posts/${post._id}`, {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${loginData.token}`,
+              },
+            });
+
+            if (response.ok) {
+              console.log("Success");
+              location.reload();
+            }
+
+            let response = await promise;
+            let data = await response.json();
+            console.log(data);
+          } catch (error) {
+            console.error;
+          }
+        }
+      });
+      // }
+
+      feed.appendChild(feedHeader);
+
+      feed.appendChild(postStructure);
+      postHeaderText.appendChild(text);
+      postHeader.appendChild(postHeaderText);
+      postHeader.appendChild(headerSpecial);
+      postBody.appendChild(postHeader);
+      postBody.appendChild(headerDescription);
+      postBody.appendChild(editPost);
+      postBody.appendChild(deleteButton);
+      // postStructure.appendChild(postAvatar);
+      // postStructure.appendChild(userNameLink);
+      postStructure.appendChild(usernameAndPic);
+      postStructure.appendChild(postBody);
+      postStructure.appendChild(options);
+      postCardArea.appendChild(postStructure);
+    });
+  }, 2000);
 }
 
 async function getOtherUsersAndPosts() {
@@ -267,3 +322,37 @@ async function getOtherUsersAndPosts() {
     console.error;
   }
 }
+
+let darkmode = localStorage.getItem("darkmode");
+let themeChanger = document.getElementById("themeChanger");
+
+function enableDarkmode() {
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+  themeChanger.innerText = "Light";
+    themeChanger.className = "btn btn-dark rounded-5 ms-2 mb-2"
+     header.className = "container-fluid bg-body-secondary text-light py-4 border"
+}
+
+function disableDarkmode() {
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+  themeChanger.innerText = "Dark";
+  themeChanger.className = "btn btn-light rounded-5 ms-2 mb-2"
+  header.className = "container-fluid bg-body-tertiary text-light py-4 border"
+}
+
+if (darkmode === "active") enableDarkmode();
+
+themeChanger.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  // darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+  // console.log(themeChanger)
+  if (darkmode !== "active") {
+    enableDarkmode();
+  } else {
+    disableDarkmode();
+  }
+});
+
+console.log(themeChanger);
